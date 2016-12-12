b1365fccf3e4747b08160adb357be9bddb09ebd2
Carlieg <carla@itsmecarla.com>
2016-12-12 04:41:13 -0800

    R program file - mlogit

--- R-Scripts/cbcjuice-Final.R
+++ R-Scripts/cbcjuice-Final.R
@@ -1,9 +1,12 @@
 ## Simulate data
+write.table(cbc.df, file = "~/datasets/orangejuicesim.csv", append = FALSE, sep = ",")
+
 
 attrib <- list(pulp = c("none", "some", "lots"), 
                juice = c("frozen", "fresh"),
-               added = c("vit_A", "vit_B", "multi"), 
-               price = c("3.89", "4.89", "5.89"))
+               added = c("vit_D", "vit_B", "multi"), 
+               price = c("3.89", "4.59", "4.89"))
+
 coef.names <- NULL
 for (a in seq_along(attrib)) {
   coef.names <- c(coef.names, 
@@ -27,8 +30,7 @@ coefs <- mvrnorm(length(resp.id), mu=mu, Sigma=Sigma)
 colnames(coefs) <- coef.names
 coefs[healthy=="yes", "addedvit_B"] <- coefs[healthy=="yes", "addedvit_B"] + 2
 coefs[healthy=="yes", "addedmulti"] <- coefs[healthy=="yes", "addedmulti"] + 1.5
-# Try it!: head(cbind(carpool, coefs))
-# Try it!: by(coefs, healthy, colMeans)
+
 nques <- 15
 nalt <- 3
 
@@ -55,33 +57,35 @@ for (i in seq_along(resp.id)) {
   cbc.df <- rbind(cbc.df, conjoint.i)
 }  
 
-# cleanup! -- not in book:
+# cleanup!:
 rm(a, i, resp.id, healthy, mu, Sigma, coefs, coef.names, attrib,
    conjoint.i, profiles, profiles.i, profiles.coded, utility, 
    wide.util, probs, choice, nalt, nques)
-# Try it!: ls()
 
 ## END data simulation
 
-
-
-
-
-
 # Choice data descriptives
+str(cbc.df)
 head(cbc.df)
 summary(cbc.df)
 
 # write.csv(cbc.df, "rintro-chapter13conjoint.csv", row.names=FALSE)
 
+summary(cbc.df)
+xtabs(choice ~ pulp, data=cbc.df)
+xtabs(choice ~ juice, data=cbc.df)
+xtabs(choice ~ added, data=cbc.df)
 xtabs(choice ~ price, data=cbc.df)
-xtabs(choice ~ cargo, data=cbc.df)
 
-# Fitting a choice model with mlogit
+
+#_______________________________________________________
+# Fitting a choice model with mlogit using the long format 
+install.packages()
 library(mlogit)
 cbc.mlogit <- mlogit.data(data=cbc.df, choice="choice", shape="long", 
                           varying=3:6, alt.levels=paste("pos",1:3), 
                           id.var="resp.id")
+
 
 m1 <- mlogit(choice ~ 0 + pulp + juice + added + price, data = cbc.mlogit)
 summary(m1)
@@ -94,175 +98,21 @@ m3 <- mlogit(choice ~ 0 + pulp + juice + added
              + as.numeric(as.character(price)), 
              data = cbc.mlogit)
 summary(m3)
+
 lrtest(m1, m3)
 
-
 # Willingness to pay
-coef(m3)["juicefrozen"]/(-coef(m3)["as.numeric(as.character(price))"]/1000)
+coef(m3)["added"]/(-coef(m3)["as.numeric(as.character(price))"]/1)
 
-# Predicting shares
 predict.mnl <- function(model, data) {
-  # Function for predicting shares from a multinomial logit model 
-  # model: mlogit object returned by mlogit()
-  # data: a data frame containing the set of designs for which you want to 
-  #       predict shares.  Same format at the data used to estimate model. 
-  data.model <- model.matrix(update(model$formula, 0 ~ .), data = data)[,-1]
-  utility <- data.model%*%model$coef
-  share <- exp(utility)/sum(exp(utility))
-  cbind(share, data)
-}
+  # Function for predicting shares from a multinomial logit model
+    # model: mlogit object returned by mlogit()
+     # data: a data frame containing the set of designs for which you want to
+     #       predict shares.  Same format as the data used to estimate model.
+    data.model <- model.matrix(update(model$formula, 0 ~ .), data = data)[,-1]
+    utility <- data.model%*%model$coef
+    share <- exp(utility)/sum(exp(utility))
+   cbind(share, data) }
 
 (new.data <- expand.grid(attrib)[c(8, 1, 3, 41, 49, 26), ])
-predict.mnl(m3, new.data)
-predict.mnl(m1, new.data)
-
-# Share sensitivity
-sensitivity.mnl <- function(model, attrib, base.data, competitor.data) {
-  # Function for creating data for a share-sensitivity chart
-  # model: mlogit object returned by mlogit() function
-  # attrib: list of vectors with attribute levels to be used in sensitivity
-  # base.data: data frame containing baseline design of target product
-  # competitor.data: data frame contining design of competitive set
-  data <- rbind(base.data, competitor.data)
-  base.share <- predict.mnl(model, data)[1,1]
-  share <- NULL
-  for (a in seq_along(attrib)) {
-    for (i in attrib[[a]]) {
-      data[1,] <- base.data
-      data[1,a] <- i
-      share <- c(share, predict.mnl(model, data)[1,1])
-    }
-  }
-  data.frame(level=unlist(attrib), share=share, increase=share-base.share)
-}
-base.data <- expand.grid(attrib)[c(8), ]
-competitor.data <- expand.grid(attrib)[c(1, 3, 41, 49, 26), ]
-(tradeoff <- sensitivity.mnl(m1, attrib, base.data, competitor.data))
-
-barplot(tradeoff$increase, horiz=FALSE, names.arg=tradeoff$level,
-        ylab="Change in Share for Baseline Product")
-
-
-# Share predictions for two identical vehicles
-new.data.2 <- expand.grid(attrib)[c(8, 8, 1, 3, 41, 49, 26), ]
-predict.mnl(m1, new.data.2)  
-
-
-# Prediction uncertainty & conjoint design
-small.cbc <- mlogit.data(data=cbc.df[1:(25*15*3),], 
-                         choice="choice", shape="long", 
-                         varying=3:6, alt.levels=paste("pos", 1:3), 
-                         id.var="resp.id")
-m4 <- mlogit(choice ~ 0 + seat + cargo + eng + price, data = small.cbc)
-summary(m4)  # larger standard errors
-
-cbind(predict.mnl(m4, new.data), predict.mnl(m1, new.data))
-
-
-# ------- Mixed logit model ------------------------------
-# Model estimation
-m1.rpar <- rep("n", length=length(m1$coef))
-names(m1.rpar) <- names(m1$coef)
-m1.rpar
-m1.hier <- mlogit(choice ~ 0 + seat + eng + cargo + price, 
-                  data = cbc.mlogit, 
-                  panel=TRUE, rpar = m1.rpar, correlation = FALSE)
-summary(m1.hier)
-
-m2.hier <- update(m1.hier, correlation = TRUE)
-summary(m2.hier)
-cov2cor(cov.mlogit(m2.hier))
-
-
-# Simulating shares
-predict.hier.mnl <- function(model, data, nresp=1000) {
-  # Function for predicting shares from a hierarchical multinomial logit model 
-  # model: mlogit object returned by mlogit()
-  # data: a data frame containing the set of designs for which you want to 
-  #       predict shares.  Same format at the data used to estimate model. 
-  # Note that this code assumes all model parameters are random
-  data.model <- model.matrix(update(model$formula, 0 ~ .), data = data)[,-1]
-  coef.Sigma <- cov.mlogit(model)
-  coef.mu <- model$coef[1:dim(coef.Sigma)[1]]
-  draws <- mvrnorm(n=nresp, coef.mu, coef.Sigma)
-  shares <- matrix(NA, nrow=nresp, ncol=nrow(data))
-  for (i in 1:nresp) {
-    utility <- data.model%*%draws[i,]
-    share = exp(utility)/sum(exp(utility))
-    shares[i,] <- share
-  }
-  cbind(colMeans(shares), data)
-}
-
-predict.hier.mnl(m2.hier, data=new.data)
-
-
-# ------- Hierarchical Bayes multinomial logit -----------
-choice <- rep(0, nrow(cbc.df))
-choice[cbc.df[,"alt"]==1] <- cbc.df[cbc.df[,"choice"]==1,"alt"]
-head(choice)
-
-cbc.coded <- model.matrix(~ seat + eng + cargo + price, data = cbc.df)
-cbc.coded <- cbc.coded[, -1] # remove the intercept
-
-choicemodelr.data <- cbind(cbc.df[, 1:3], cbc.coded, choice)
-head(choicemodelr.data)
-
-carpool <- cbc.df$carpool[cbc.df$ques==1 & cbc.df$alt==1]=="yes"
-carpool <- as.numeric(carpool)
-choicemodelr.demos <- as.matrix(carpool, nrow=length(carpool))
-str(choicemodelr.demos)
-
-library(ChoiceModelR)
-hb.post <- choicemodelr(data=choicemodelr.data, xcoding=rep(1, 7), 
-                        demos=choicemodelr.demos, 
-                        mcmc=list(R=20000, use=10000),
-                        options=list(save=TRUE))
-
-names(hb.post)
-
-# Model parameters
-hb.post$compdraw[[567]]$mu
-hb.post$deltadraw[567,]
-
-hb.post$compdraw[[567]]$rooti
-crossprod(hb.post$compdraw[[567]]$rooti)
-
-# Individual-level betas
-head(hb.post$betadraw[,,567])
-str(hb.post$betadraw)
-
-beta.post.mean <- apply(hb.post$betadraw, 1:2, mean)
-head(beta.post.mean)
-
-beta.post.q05 <- apply(hb.post$betadraw, 1:2, quantile, probs=c(0.05))
-beta.post.q95 <- apply(hb.post$betadraw, 1:2, quantile, probs=c(0.95))
-rbind(q05=beta.post.q05[1,], mean=beta.post.mean[1,], q95=beta.post.q95[1,])
-
-# Prediction using individual-level draws
-predict.hb.mnl <- function(betadraws, data) {
-  # Function for predicting shares from a hierarchical multinomial logit model 
-  # model: mlogit object returned by mlogit()
-  # data: a data frame containing the set of designs for which you want to 
-  #       predict shares.  Same format at the data used to estimate model. 
-  data.model <- model.matrix(~ seat + eng + cargo + price, data = data)
-  data.model <- data.model[,-1] # remove the intercept
-  nresp <- dim(betadraws)[1]
-  ndraws <- dim(hb.post$betadraw)[3]
-  shares <- array(dim=c(nresp, nrow(data), ndraws))
-  for (d in 1:ndraws) {
-    for (i in 1:nresp) {
-      utility <- data.model%*%betadraws[i,,d]
-      shares[i,,d] = exp(utility)/sum(exp(utility))
-    }
-  }
-  shares.agg <- apply(shares, 2:3, mean)
-  cbind(share=apply(shares.agg, 1, mean), 
-        pct=t(apply(shares.agg, 1, quantile, probs=c(0.05, 0.95))), 
-        data)
-}
-
-predict.hb.mnl(hb.post$betadraw, new.data)
-# Try it!
-# new.data[2,1] <- 7
-# predict.hb.mnl(hb.post$betadraw, new.data)
+(new.data1 <- expand.grid(attrib)[c(18, 1, 3, 22, 49, 29), ])
